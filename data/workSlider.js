76fc19845d96ef2e27fbd106b7394a1d92e1b20f
Chris Warren <chris.warren.2019@gmail.com>
2023-08-05 10:52:06 -0400

    Updated the pictures on my portfolio & removed some images that were no longer needed

--- data/workSlider.js
+++ data/workSlider.js
@@ -4,19 +4,27 @@ export const workSlider = {
       images: [
         {
           title: "Amazon (Look-alike)",
-          path: "/images/projects/React/amazon-look-a-like.webp",
+          path: "/images/projects/React/Amazon.webp",
+          link: "https://clone-ce73b.web.app/",
+          github: "https://github.com/ItsCDubb/amazon-look-a-like-front-end",
         },
         {
           title: "Slack (Look-alike)",
-          path: "/images/projects/React/slack-look-a-like.webp",
+          path: "/images/projects/React/Slack.webp",
+          link: "https://slack-look-a-like.web.app/",
+          github: "https://github.com/ItsCDubb/slack-look-a-like-front-end",
         },
         {
           title: "This Website",
-          path: "/images/projects/Next/this-website.webp",
+          path: "/images/projects/Next/ChrisWarren.webp",
+          link: "https://www.chriswarren.dev",
+          github: "https://github.com/ItsCDubb/amazon-look-a-like-front-end",
         },
         {
           title: "Netflix (Look-alike)",
-          path: "/images/projects/React/netflix-look-a-like.webp",
+          path: "/images/projects/React/Netflix.webp",
+          link: "https://netflix-clone-fe7b3.web.app/",
+          github: "https://github.com/ItsCDubb/Netflix",
         },
       ],
     },
@@ -24,11 +32,15 @@ export const workSlider = {
       images: [
         {
           title: "CW Technologies",
-          path: "/images/projects/React/cwtechnologies.webp",
+          path: "/images/projects/React/CWTechnologies.webp",
+          link: "https://cwtechnologies.co/",
+          github: "https://github.com/ItsCdubb",
         },
         {
           title: "Twitter (Look-alike)",
-          path: "/images/projects/React/twitter-look-a-like.webp",
+          path: "/images/projects/React/Twitter.webp",
+          link: "https://twitter-look-a-like.web.app/",
+          github: "https://github.com/ItsCDubb/twitter-look-a-like",
         },
       ],
     },
