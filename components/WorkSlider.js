5b0282b4084434a7bce1ad692d0dfd866dfdad9f
Chris Warren <chris.warren.2019@gmail.com>
2023-08-05 11:25:14 -0400

    Updated the links to my portfolio to include the Github link

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -4,6 +4,7 @@ import { BsArrowRight } from "react-icons/bs";
 import { Pagination } from "swiper";
 import Image from "next/image";
 import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
@@ -29,15 +30,25 @@ const WorkSlider = () => {
                     key={index}
                   >
                     <div className="flex items-center justify-center relative overflow-hidden group">
-                      <Image src={image.path} width={500} height={200} alt="" />
+                      <Image
+                        src={image.path}
+                        width={500}
+                        height={200}
+                        alt={image.title}
+                      />
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-primary to-accent opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          <div className="delay-100">View</div>
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                            <BsArrowRight />
+                        <Link key={index} href={image.link} target="_Blank">
+                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                            <div className="delay-100">View</div>
+                            <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                              <BsArrowRight />
+                            </div>
                           </div>
-                        </div>
+                        </Link>
+                        <Link key={index} href={image.github} target="_Blank">
+                          <BsGithub />
+                        </Link>
                       </div>
                     </div>
                   </div>
